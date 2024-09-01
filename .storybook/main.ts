3a504247ac0c298a78830a7282527513f2f306eb
Brandon Roberts <robertsbt@gmail.com>
2024-09-01 15:20:28 -0500

    feat: update to 1.8.0-beta.1 with Storybook detection

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -24,20 +24,6 @@ const config = {
     const { mergeConfig } = await import('vite');
     const { default: angular } = await import('@analogjs/vite-plugin-angular');
 
-    /**
-     * Replace imports of "@storybook/angular" with "@storybook/angular/dist/client"
-     */
-    const storybookAngularImportPlugin = () => ({
-      name: '@storybook/angular',
-      transform(code: string) {
-        if (code.includes('"@storybook/angular"')) {
-          return code.replace(/\"@storybook\/angular\"/g, '\"@storybook/angular/dist/client\"');
-        }
-
-        return;
-      }
-    });
-
     return mergeConfig(config, {
       // Add dependencies to pre-optimization
       optimizeDeps: {
@@ -51,8 +37,7 @@ const config = {
         ]
       },
       plugins: [
-        angular({ jit: true, tsconfig: './.storybook/tsconfig.json' }),
-        storybookAngularImportPlugin()
+        angular({ jit: true, tsconfig: './.storybook/tsconfig.json' })
       ]
     });
   },
